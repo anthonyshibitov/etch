dd26f7e212036e1b055130c9d0cc0bf0a4f338d5
Anthony Shibitov <anthonyshibitov@gmail.com>
2022-09-06 22:34:35 -0400

    Add logic for button and slider
    Logic now exists on button click to draw a square of elements
    (currently text, as 'x's)

--- source.js
+++ source.js
@@ -1,12 +1,32 @@
 let slider = document.querySelector('#sizeSlider');
+let redraw = document.querySelector('#redrawButton');
 let sliderValue = document.querySelector('.slider-value');
+let testScreen = document.querySelector('.test-screen');
+
 sliderValue.textContent = slider.value;
+
+//Allow carraige returns using \r\n to be added
+sliderValue.setAttribute('style', 'white-space: pre;');
+testScreen.setAttribute('style', 'white-space: pre;');
+
 
 slider.addEventListener('input', (e) => {
     sliderValue.textContent = slider.value;
+});
+
+redraw.addEventListener('click', (e) => {
     redrawScreen(slider.value);
 });
 
 function redrawScreen(size){
-    sliderValue.textContent += `, draw ${size}x${size} screen!`;
+    testScreen.textContent = '';
+    testScreen.textContent += `\r\nDraw ${size}x${size} screen!\r\n\r\n`;
+
+    //Loop prototype
+    for(let i = 0; i < size; i++){
+        for(let j = 0; j < size; j++){
+            testScreen.textContent += 'x';
+        }
+        testScreen.textContent += `\r\n`;
+    }
 }
